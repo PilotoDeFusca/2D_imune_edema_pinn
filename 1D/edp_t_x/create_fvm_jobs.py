411749170230fd39e17f98ac874bb344aaf9d0d5
PilotoDeFusca <ph4581@gmail.com>
2025-09-17 16:19:01 -0300

    Revert "teste"
    This reverts commit c345226b4153b382406710f0f3a300a2443a65da.

--- 1D/edp_t_x/create_fvm_jobs.py
+++ 1D/edp_t_x/create_fvm_jobs.py
@@ -1,8 +1,16 @@
 import argparse
 import os
-from parametros_maquina import *
 
 
+v_gpu = [
+    "MIG-a444fcc0-f725-530b-9ffb-97805cefb734",
+    "MIG-10685134-19fb-5361-83da-7bdc9b8242ba",
+    "MIG-a5ff4856-76ba-5d4a-bc36-d6c908a95b14",
+    "MIG-d65b56b1-2519-5354-96ae-aec5f0e41128",
+    "MIG-275c5d7e-981d-5f7a-b45b-0659ba9ad13a",
+    "MIG-3aad3b21-c6f1-5b32-9d6b-1341d2b38d11",
+    "MIG-f946a009-bfbb-5335-89ba-7f3ac431bf10",
+]
 
 parser = argparse.ArgumentParser(description="", add_help=False)
 parser = argparse.ArgumentParser()
@@ -71,11 +79,11 @@ def write_setup():
         "jobs/fvm_comp.job",
     )
     add_line(
-        f"# Run time (hh:mm:ss) - {tempo_execução}",
+        "# Run time (hh:mm:ss) - 10:00 hr",
         "jobs/fvm_comp.job",
     )
     add_line(
-        f"#PBS -l walltime={tempo_execução}",
+        "#PBS -l walltime=10:00:00",
         "jobs/fvm_comp.job",
     )
     add_line(
@@ -83,7 +91,7 @@ def write_setup():
         "jobs/fvm_comp.job",
     )
     add_line(
-        f"#PBS -l nodes=compute-{maquina}:ppn={numero_nucleos}",
+        "#PBS -l nodes=compute-1-1:ppn=128",
         "jobs/fvm_comp.job",
     )
     add_line(
@@ -103,7 +111,7 @@ def write_setup():
         "jobs/fvm_comp.job",
     )
     add_line(
-        f"export CUDA_VISIBLE_DEVICES={gpu_id}",
+        "export CUDA_VISIBLE_DEVICES=GPU-fd7e14c3-91ce-6c4b-e736-393c0d0537ef",
         "jobs/fvm_comp.job",
     )
 
